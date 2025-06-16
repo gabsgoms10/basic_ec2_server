2615570254b72789c21c5cfc426b58c989301e31
Gabriel Cipriano <77gabrielgomes2001@gmail.com>
2025-06-16 17:16:18 -0300

    adicao_subnet_vpc

--- main.tf
+++ main.tf
@@ -2,3 +2,17 @@ provider "aws" {
     region = "us-east-1"
 }
 
+resource "aws_vpc" "central_vpc"{
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "development_vpc"
+    }
+}
+
+resource "aws_subnet" "subnet_1"{
+    vpc_id = aws_vpc.central_vpc.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "development_subnet"
+    }
+}
